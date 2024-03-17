816fb73d143b8d34c56f62d10dbc2980cc74d16f
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-03-17 15:51:20 +0530

    add local url

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ const app = express()
 app.use(express.json()); // For parsing application/json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
+
+app.use(cors())
+app.use(cors({origin: "http://127.0.0.1:5500/frontend/test.html"}))
+
+
 import userRouter from './routes/user.routes.js'
 
 app.use(cors())
